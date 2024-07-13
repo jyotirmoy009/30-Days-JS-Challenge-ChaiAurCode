313121a6b763a22a331c0e3d6ba18c5ea9210738
Jyotirmoy <jyotirmoysmart121@gmail.com>
2024-07-14 00:51:10 +0530

    initial commit

---
+++ Day-1.js
@@ -0,0 +1,48 @@
+// Activity 1 
+// Task 1
+var num = 9;
+console.log(num);
+
+// Task2
+let name = 'Jyotirmoy';
+console.log(name);
+
+// Activity 3
+// TAsk 3
+const value = true;
+console.log(value);
+
+// Activity 3
+// Task 4
+let number = 5;
+let string = 'Jyotirmoy'
+let boolean = true;
+let obj = {
+    name : 'Jyotirmoy',
+    username : 'jyotirmoy7754',
+    age : 21
+}
+let array = [1,2,'true','shaktiman',5];
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof obj);
+console.log(typeof array);
+
+// Activity 5
+// Task 5
+
+let firstValue = 10;
+console.log(firstValue);
+let secondValue = 20;
+console.log(secondValue);
+
+// Activity 6
+// Task 6
+
+const valueOne = 200;
+// valueOne = 400;
+console.log(valueOne); //error - TypeError: Assignment to constant variable.
+
+
+
